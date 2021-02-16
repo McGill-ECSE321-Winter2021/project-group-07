c85b0066c4230f6445438531879608faf290201f
Catherine Van Gheluwe <59707834+kitcath@users.noreply.github.com>
2021-02-16 16:06:27 -0500

    JPA Annotations for RepairSystem.java

--- RepairSystem-Backend/src/main/java/ca/mcgill/ecse321/repairsystem/model/RepairSystem.java
+++ RepairSystem-Backend/src/main/java/ca/mcgill/ecse321/repairsystem/model/RepairSystem.java
@@ -1,541 +1,114 @@
 package ca.mcgill.ecse321.repairsystem.model;
 
 import java.util.*;
-import java.sql.Time;
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
+import javax.persistence.Entity;
 
-// line 2 "model.ump"
-// line 90 "model.ump"
+
+@Entity
 public class RepairSystem
 {
 
-  //------------------------
-  // MEMBER VARIABLES
-  //------------------------
-
-  //RepairSystem Attributes
   private String shopName;
-  private String address;
-  private int spaceAvailable;
-  private int numOfEmployees;
-
-  //RepairSystem Associations
-  private List<User> users;
-  private List<Service> services;
-  private List<Appointment> appointments;
-  private List<TimeSlot> timeSlots;
-
-  //------------------------
-  // CONSTRUCTOR
-  //------------------------
-
-  public RepairSystem(String aShopName, String aAddress, int aSpaceAvailable, int aNumOfEmployees)
-  {
-    shopName = aShopName;
-    address = aAddress;
-    spaceAvailable = aSpaceAvailable;
-    numOfEmployees = aNumOfEmployees;
-    users = new ArrayList<User>();
-    services = new ArrayList<Service>();
-    appointments = new ArrayList<Appointment>();
-    timeSlots = new ArrayList<TimeSlot>();
-  }
-
-  //------------------------
-  // INTERFACE
-  //------------------------
-
-  public boolean setShopName(String aShopName)
-  {
-    boolean wasSet = false;
-    shopName = aShopName;
-    wasSet = true;
-    return wasSet;
-  }
-
-  public boolean setAddress(String aAddress)
-  {
-    boolean wasSet = false;
-    address = aAddress;
-    wasSet = true;
-    return wasSet;
-  }
-
-  public boolean setSpaceAvailable(int aSpaceAvailable)
-  {
-    boolean wasSet = false;
-    spaceAvailable = aSpaceAvailable;
-    wasSet = true;
-    return wasSet;
-  }
-
-  public boolean setNumOfEmployees(int aNumOfEmployees)
-  {
-    boolean wasSet = false;
-    numOfEmployees = aNumOfEmployees;
-    wasSet = true;
-    return wasSet;
-  }
-
+  
+  
   public String getShopName()
   {
-    return shopName;
+    return this.shopName;
+  }
+  
+  public void setShopName(String aShopName)
+  {
+    this.shopName = aShopName;
   }
 
+  private String address;
+  
   public String getAddress()
   {
-    return address;
+    return this.address;
   }
 
+  public void setAddress(String aAddress)
+  {
+    this.address = aAddress;
+  }
+  
+  private int spaceAvailable;
+  
+  
   public int getSpaceAvailable()
   {
-    return spaceAvailable;
+    return this.spaceAvailable;
   }
 
+  public void setSpaceAvailable(int aSpaceAvailable)
+  {
+    this.spaceAvailable=aSpaceAvailable;
+  }
+  
+  private int numOfEmployees;
+  
+  
+  
   public int getNumOfEmployees()
   {
-    return numOfEmployees;
+    return this.numOfEmployees;
   }
-  /* Code from template association_GetMany */
-  public User getUser(int index)
+  
+  public void setNumOfEmployees(int aNumOfEmployees)
   {
-    User aUser = users.get(index);
-    return aUser;
+    this.numOfEmployees=aNumOfEmployees;
   }
 
+  //RepairSystem Associations
+  private List<User> user;
+  
+  @OneToMany(cascade={CascadeType.ALL})
   public List<User> getUsers()
   {
-    List<User> newUsers = Collections.unmodifiableList(users);
-    return newUsers;
+    return this.user;
   }
 
-  public int numberOfUsers()
-  {
-    int number = users.size();
-    return number;
+ public void setUser(List<User> users) {
+	  this.user = users;
   }
 
-  public boolean hasUsers()
-  {
-    boolean has = users.size() > 0;
-    return has;
-  }
-
-  public int indexOfUser(User aUser)
-  {
-    int index = users.indexOf(aUser);
-    return index;
-  }
-  /* Code from template association_GetMany */
-  public Service getService(int index)
-  {
-    Service aService = services.get(index);
-    return aService;
-  }
-
+  private List<Service> service;
+  
+  @OneToMany(cascade={CascadeType.ALL})
   public List<Service> getServices()
   {
-    List<Service> newServices = Collections.unmodifiableList(services);
-    return newServices;
+    return this.service;
+  }
+  
+  public void setServices(List<Service> services) {
+	  this.service = services;
   }
 
-  public int numberOfServices()
-  {
-    int number = services.size();
-    return number;
-  }
-
-  public boolean hasServices()
-  {
-    boolean has = services.size() > 0;
-    return has;
-  }
-
-  public int indexOfService(Service aService)
-  {
-    int index = services.indexOf(aService);
-    return index;
-  }
-  /* Code from template association_GetMany */
-  public Appointment getAppointment(int index)
-  {
-    Appointment aAppointment = appointments.get(index);
-    return aAppointment;
-  }
-
+  private List<Appointment> appointment;
+  
+  @OneToMany(cascade={CascadeType.ALL})
   public List<Appointment> getAppointments()
   {
-    List<Appointment> newAppointments = Collections.unmodifiableList(appointments);
-    return newAppointments;
+    return this.appointment;
   }
-
-  public int numberOfAppointments()
-  {
-    int number = appointments.size();
-    return number;
+  
+  public void setAppointments(List<Appointment> appointments) {
+	  this.appointment=appointments;
   }
-
-  public boolean hasAppointments()
-  {
-    boolean has = appointments.size() > 0;
-    return has;
-  }
-
-  public int indexOfAppointment(Appointment aAppointment)
-  {
-    int index = appointments.indexOf(aAppointment);
-    return index;
-  }
-  /* Code from template association_GetMany */
-  public TimeSlot getTimeSlot(int index)
-  {
-    TimeSlot aTimeSlot = timeSlots.get(index);
-    return aTimeSlot;
-  }
-
+  
+  private List<TimeSlot> timeSlot;
+  @OneToMany(cascade={CascadeType.ALL})
   public List<TimeSlot> getTimeSlots()
   {
-    List<TimeSlot> newTimeSlots = Collections.unmodifiableList(timeSlots);
-    return newTimeSlots;
+    return timeSlot;
   }
 
-  public int numberOfTimeSlots()
-  {
-    int number = timeSlots.size();
-    return number;
+  public void setTimeSlot(List<TimeSlot> timeSlots) {
+	  this.timeSlot=timeSlots;
   }
-
-  public boolean hasTimeSlots()
-  {
-    boolean has = timeSlots.size() > 0;
-    return has;
-  }
-
-  public int indexOfTimeSlot(TimeSlot aTimeSlot)
-  {
-    int index = timeSlots.indexOf(aTimeSlot);
-    return index;
-  }
-  /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfUsers()
-  {
-    return 0;
-  }
-  /* Code from template association_AddManyToOne */
-  public User addUser(String aName, int aId, String aPassword, int aPhone, String aEmail)
-  {
-    return new User(aName, aId, aPassword, aPhone, aEmail, this);
-  }
-
-  public boolean addUser(User aUser)
-  {
-    boolean wasAdded = false;
-    if (users.contains(aUser)) { return false; }
-    RepairSystem existingRepairSystem = aUser.getRepairSystem();
-    boolean isNewRepairSystem = existingRepairSystem != null && !this.equals(existingRepairSystem);
-    if (isNewRepairSystem)
-    {
-      aUser.setRepairSystem(this);
-    }
-    else
-    {
-      users.add(aUser);
-    }
-    wasAdded = true;
-    return wasAdded;
-  }
-
-  public boolean removeUser(User aUser)
-  {
-    boolean wasRemoved = false;
-    //Unable to remove aUser, as it must always have a repairSystem
-    if (!this.equals(aUser.getRepairSystem()))
-    {
-      users.remove(aUser);
-      wasRemoved = true;
-    }
-    return wasRemoved;
-  }
-  /* Code from template association_AddIndexControlFunctions */
-  public boolean addUserAt(User aUser, int index)
-  {  
-    boolean wasAdded = false;
-    if(addUser(aUser))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfUsers()) { index = numberOfUsers() - 1; }
-      users.remove(aUser);
-      users.add(index, aUser);
-      wasAdded = true;
-    }
-    return wasAdded;
-  }
-
-  public boolean addOrMoveUserAt(User aUser, int index)
-  {
-    boolean wasAdded = false;
-    if(users.contains(aUser))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfUsers()) { index = numberOfUsers() - 1; }
-      users.remove(aUser);
-      users.add(index, aUser);
-      wasAdded = true;
-    } 
-    else 
-    {
-      wasAdded = addUserAt(aUser, index);
-    }
-    return wasAdded;
-  }
-  /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfServices()
-  {
-    return 0;
-  }
-  /* Code from template association_AddManyToOne */
-  public Service addService(Service.ServiceType aType, int aPrice)
-  {
-    return new Service(aType, aPrice, this);
-  }
-
-  public boolean addService(Service aService)
-  {
-    boolean wasAdded = false;
-    if (services.contains(aService)) { return false; }
-    RepairSystem existingRepairSystem = aService.getRepairSystem();
-    boolean isNewRepairSystem = existingRepairSystem != null && !this.equals(existingRepairSystem);
-    if (isNewRepairSystem)
-    {
-      aService.setRepairSystem(this);
-    }
-    else
-    {
-      services.add(aService);
-    }
-    wasAdded = true;
-    return wasAdded;
-  }
-
-  public boolean removeService(Service aService)
-  {
-    boolean wasRemoved = false;
-    //Unable to remove aService, as it must always have a repairSystem
-    if (!this.equals(aService.getRepairSystem()))
-    {
-      services.remove(aService);
-      wasRemoved = true;
-    }
-    return wasRemoved;
-  }
-  /* Code from template association_AddIndexControlFunctions */
-  public boolean addServiceAt(Service aService, int index)
-  {  
-    boolean wasAdded = false;
-    if(addService(aService))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfServices()) { index = numberOfServices() - 1; }
-      services.remove(aService);
-      services.add(index, aService);
-      wasAdded = true;
-    }
-    return wasAdded;
-  }
-
-  public boolean addOrMoveServiceAt(Service aService, int index)
-  {
-    boolean wasAdded = false;
-    if(services.contains(aService))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfServices()) { index = numberOfServices() - 1; }
-      services.remove(aService);
-      services.add(index, aService);
-      wasAdded = true;
-    } 
-    else 
-    {
-      wasAdded = addServiceAt(aService, index);
-    }
-    return wasAdded;
-  }
-  /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfAppointments()
-  {
-    return 0;
-  }
-  /* Code from template association_AddManyToOne */
-  public Appointment addAppointment(String aNote, Appointment.AppointmentStatus aStatus, TimeSlot aAppointmentTime, Customer aCustomer, Car aCar, Mechanic[] allMechanics, Service[] allRequireServices)
-  {
-    return new Appointment(aNote, aStatus, aAppointmentTime, aCustomer, this, aCar, allMechanics, allRequireServices);
-  }
-
-  public boolean addAppointment(Appointment aAppointment)
-  {
-    boolean wasAdded = false;
-    if (appointments.contains(aAppointment)) { return false; }
-    RepairSystem existingRepairSystem = aAppointment.getRepairSystem();
-    boolean isNewRepairSystem = existingRepairSystem != null && !this.equals(existingRepairSystem);
-    if (isNewRepairSystem)
-    {
-      aAppointment.setRepairSystem(this);
-    }
-    else
-    {
-      appointments.add(aAppointment);
-    }
-    wasAdded = true;
-    return wasAdded;
-  }
-
-  public boolean removeAppointment(Appointment aAppointment)
-  {
-    boolean wasRemoved = false;
-    //Unable to remove aAppointment, as it must always have a repairSystem
-    if (!this.equals(aAppointment.getRepairSystem()))
-    {
-      appointments.remove(aAppointment);
-      wasRemoved = true;
-    }
-    return wasRemoved;
-  }
-  /* Code from template association_AddIndexControlFunctions */
-  public boolean addAppointmentAt(Appointment aAppointment, int index)
-  {  
-    boolean wasAdded = false;
-    if(addAppointment(aAppointment))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfAppointments()) { index = numberOfAppointments() - 1; }
-      appointments.remove(aAppointment);
-      appointments.add(index, aAppointment);
-      wasAdded = true;
-    }
-    return wasAdded;
-  }
-
-  public boolean addOrMoveAppointmentAt(Appointment aAppointment, int index)
-  {
-    boolean wasAdded = false;
-    if(appointments.contains(aAppointment))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfAppointments()) { index = numberOfAppointments() - 1; }
-      appointments.remove(aAppointment);
-      appointments.add(index, aAppointment);
-      wasAdded = true;
-    } 
-    else 
-    {
-      wasAdded = addAppointmentAt(aAppointment, index);
-    }
-    return wasAdded;
-  }
-  /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfTimeSlots()
-  {
-    return 0;
-  }
-  /* Code from template association_AddManyToOne */
-  public TimeSlot addTimeSlot(Time aStartTime, Time aEndTime)
-  {
-    return new TimeSlot(aStartTime, aEndTime, this);
-  }
-
-  public boolean addTimeSlot(TimeSlot aTimeSlot)
-  {
-    boolean wasAdded = false;
-    if (timeSlots.contains(aTimeSlot)) { return false; }
-    RepairSystem existingRepairSystem = aTimeSlot.getRepairSystem();
-    boolean isNewRepairSystem = existingRepairSystem != null && !this.equals(existingRepairSystem);
-    if (isNewRepairSystem)
-    {
-      aTimeSlot.setRepairSystem(this);
-    }
-    else
-    {
-      timeSlots.add(aTimeSlot);
-    }
-    wasAdded = true;
-    return wasAdded;
-  }
-
-  public boolean removeTimeSlot(TimeSlot aTimeSlot)
-  {
-    boolean wasRemoved = false;
-    //Unable to remove aTimeSlot, as it must always have a repairSystem
-    if (!this.equals(aTimeSlot.getRepairSystem()))
-    {
-      timeSlots.remove(aTimeSlot);
-      wasRemoved = true;
-    }
-    return wasRemoved;
-  }
-  /* Code from template association_AddIndexControlFunctions */
-  public boolean addTimeSlotAt(TimeSlot aTimeSlot, int index)
-  {  
-    boolean wasAdded = false;
-    if(addTimeSlot(aTimeSlot))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfTimeSlots()) { index = numberOfTimeSlots() - 1; }
-      timeSlots.remove(aTimeSlot);
-      timeSlots.add(index, aTimeSlot);
-      wasAdded = true;
-    }
-    return wasAdded;
-  }
-
-  public boolean addOrMoveTimeSlotAt(TimeSlot aTimeSlot, int index)
-  {
-    boolean wasAdded = false;
-    if(timeSlots.contains(aTimeSlot))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfTimeSlots()) { index = numberOfTimeSlots() - 1; }
-      timeSlots.remove(aTimeSlot);
-      timeSlots.add(index, aTimeSlot);
-      wasAdded = true;
-    } 
-    else 
-    {
-      wasAdded = addTimeSlotAt(aTimeSlot, index);
-    }
-    return wasAdded;
-  }
-
-  public void delete()
-  {
-    while (users.size() > 0)
-    {
-      User aUser = users.get(users.size() - 1);
-      aUser.delete();
-      users.remove(aUser);
-    }
-    
-    while (services.size() > 0)
-    {
-      Service aService = services.get(services.size() - 1);
-      aService.delete();
-      services.remove(aService);
-    }
-    
-    while (appointments.size() > 0)
-    {
-      Appointment aAppointment = appointments.get(appointments.size() - 1);
-      aAppointment.delete();
-      appointments.remove(aAppointment);
-    }
-    
-    while (timeSlots.size() > 0)
-    {
-      TimeSlot aTimeSlot = timeSlots.get(timeSlots.size() - 1);
-      aTimeSlot.delete();
-      timeSlots.remove(aTimeSlot);
-    }
-    
-  }
-
 
   public String toString()
   {
