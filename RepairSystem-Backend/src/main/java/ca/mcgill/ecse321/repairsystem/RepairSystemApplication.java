d3ccdae18a753c2c38bb8718f4c56b0fa7c8dde3
SiaH319 <59781764+SiaH319@users.noreply.github.com>
2021-02-16 13:37:02 -0500

    build gradle

---
+++ RepairSystem-Backend/src/main/java/ca/mcgill/ecse321/repairsystem/RepairSystemApplication.java
@@ -0,0 +1,22 @@
+package ca.mcgill.ecse321.repairsystem;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication
+public class RepairSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RepairSystemApplication.class, args);
+  }
+
+  @RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
+  }
+
+}
