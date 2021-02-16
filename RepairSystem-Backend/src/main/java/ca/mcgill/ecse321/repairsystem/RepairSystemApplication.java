487582142b80f2506e602daf165364a7098a7af4
SiaH319 <59781764+SiaH319@users.noreply.github.com>
2021-02-16 15:28:58 -0500

    build heroku

--- RepairSystem-Backend/src/main/java/ca/mcgill/ecse321/repairsystem/RepairSystemApplication.java
+++ RepairSystem-Backend/src/main/java/ca/mcgill/ecse321/repairsystem/RepairSystemApplication.java
@@ -1,6 +1,5 @@
 package ca.mcgill.ecse321.repairsystem;
 
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RestController;
