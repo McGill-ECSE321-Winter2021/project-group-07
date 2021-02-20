5e1ac2fd3e0d465495793b95e8df1bb616d25826
kitcath <catherine.vangheluwe@mail.mcgill.ca>
2021-02-19 19:14:58 -0500

    merging with master

--- RepairSystem-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/dao/AdministrativeAssistantRepository.java
+++ RepairSystem-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/dao/AdministrativeAssistantRepository.java
@@ -1,5 +1,4 @@
 package ca.mcgill.ecse321.eventregistration.dao;
-
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.repairsystem.model.AdministrativeAssistant;
