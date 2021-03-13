14b1d1c97f0aed024d0cc6b629e38cfe194768c7
SiaH319 <59781764+SiaH319@users.noreply.github.com>
2021-03-13 14:05:28 -0500

    split original tester file by classes for unit testing

---
+++ RepairSystem-Backend/src/main/java/ca/mcgill/ecse321/repairsystem/service/TestAppointmentService.java
@@ -0,0 +1,92 @@
+package ca.mcgill.ecse321.repairsystem.service;
+
+import java.util.*;
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.repairsystem.dao.*;
+import ca.mcgill.ecse321.repairsystem.model.*;
+import ca.mcgill.ecse321.repairsystem.model.Appointment.AppointmentStatus;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+public class TestAppointmentService {
+	////////////////////SERVICE APPOINTMENT METHODS //////////////////// 
+	@Autowired
+	private AppointmentRepository appointmentRepository;
+	
+	@Transactional 
+	public Appointment createApp(Customer customer, TimeSlot time, List<Mechanic> mechanics, Car car, List<Image> images, List<ca.mcgill.ecse321.repairsystem.model.Service> services, String note, AppointmentStatus status) {
+		int id = customer.hashCode() * time.hashCode();
+		Appointment app = new Appointment(customer, id, time, mechanics, car, images, services, note, status);
+		appointmentRepository.save(app);
+		return app;
+	}
+
+	@Transactional
+	public List<Appointment> getAppointmentsByCustomer(Customer customer) {
+		List<Appointment> appointments = toList(appointmentRepository.findByCustomer(customer));
+		return appointments;
+	}
+
+	@Transactional
+	public List<Appointment> getAppointmentsByCar(Car car) {
+		List<Appointment> appointments = toList(appointmentRepository.findByCar(car));
+		return appointments;
+	}
+
+	@Transactional
+	public List<Appointment> getAppointmentsByStatus(AppointmentStatus status) {
+		List<Appointment> appointments = toList(appointmentRepository.findByStatus(status));
+		return appointments;
+	}
+
+	@Transactional
+	public List<Appointment> getAppointmentsByTimeSlot(TimeSlot time) {
+		List<Appointment> appointments = toList(appointmentRepository.findByTimeSlot(time));
+		return appointments;
+	}
+
+	@Transactional
+	public Appointment getAppointmentsById(int id) {
+		Appointment appointment = appointmentRepository.findById(id);
+		return appointment;
+	}
+
+	@Transactional
+	public List<Appointment> getAllAppointments() {
+		return toList(appointmentRepository.findAll());
+	}
+
+	/* 
+	 * helper method
+	 */
+	private <T> List<T> toList(Iterable<T> iterable){
+		List<T> resultList = new ArrayList<T>();
+		for (T t : iterable) {
+			resultList.add(t);
+		}
+		return resultList;
+	}
+}
---
+++ RepairSystem-Backend/src/main/java/ca/mcgill/ecse321/repairsystem/service/TestCustomerService.java
@@ -0,0 +1,102 @@
+package ca.mcgill.ecse321.repairsystem.service;
+
+import java.util.*;
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.repairsystem.dao.*;
+import ca.mcgill.ecse321.repairsystem.model.*;
+import ca.mcgill.ecse321.repairsystem.model.Appointment.AppointmentStatus;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ca.mcgill.ecse321.repairsystem.dao.CarRepository;
+import ca.mcgill.ecse321.repairsystem.dao.CustomerRepository;
+import ca.mcgill.ecse321.repairsystem.dao.ImageRepository;
+import ca.mcgill.ecse321.repairsystem.dao.MechanicRepository;
+import ca.mcgill.ecse321.repairsystem.dao.ServiceRepository;
+import ca.mcgill.ecse321.repairsystem.dao.TimeSlotRepository;
+
+public class TestCustomerService {
+
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	////////////////////SERVICE CUSTOMER METHODS //////////////////// 
+
+	@Transactional
+	public Customer createCustomer(String aName, String aPassword, int aPhone, String aEmail, Calendar lastDate, RepairSystem aRepairSystem, String credit, String debit, String add) {
+		int id = aName.hashCode() * aPassword.hashCode();
+		Customer customer = new Customer(aName, id, aPassword, aPhone, aEmail, lastDate, aRepairSystem, credit, debit, add);
+		customerRepository.save(customer);
+		return customer;
+	}
+
+	@Transactional 
+	public Customer getCustomerById(int id) {
+		Customer customer = customerRepository.findById(id);
+		return customer;
+	}
+
+	@Transactional 
+	public List<Customer> getCustomersByName(String name) {
+		List<Customer> customers = toList(customerRepository.findByName(name));
+		return customers;
+	}
+
+	@Transactional 
+	public Customer getCustomerByNumber(int number) {
+		Customer customer = customerRepository.findByPhone(number);
+		return customer;
+	}
+
+	@Transactional 
+	public Customer getCustomerByEmail(String email) {
+		Customer customer = customerRepository.findByEmail(email);
+		return customer;
+	}
+
+	@Transactional 
+	public List<Customer> getCustomersByAddress(String address) {
+		List<Customer> customers = toList(customerRepository.findByAddress(address));
+		return customers;
+	}
+
+	@Transactional
+	public List<Customer> getAllCustomers() {
+		return toList(customerRepository.findAll());
+	}
+
+	/* 
+	 * helper method
+	 */
+	private <T> List<T> toList(Iterable<T> iterable){
+		List<T> resultList = new ArrayList<T>();
+		for (T t : iterable) {
+			resultList.add(t);
+		}
+		return resultList;
+	}
+}
