51b3587b27ef04cb0c3fdae34314e5179267ef58
kitcath <catherine.vangheluwe@mail.mcgill.ca>
2021-03-13 22:47:54 -0500

    service unit tests

--- RepairSystem-Backend/src/test/java/ca/mcgill/ecse321/repairsystem/service/TestServiceService.java
+++ RepairSystem-Backend/src/test/java/ca/mcgill/ecse321/repairsystem/service/TestServiceService.java
@@ -1,17 +1,13 @@
 package ca.mcgill.ecse321.repairsystem.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.when;
 
-import java.sql.Date;
-import java.sql.Time;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,6 +20,7 @@ import org.mockito.stubbing.Answer;
 
 import ca.mcgill.ecse321.repairsystem.dao.*;
 import ca.mcgill.ecse321.repairsystem.model.*;
+import ca.mcgill.ecse321.repairsystem.model.Service.ServiceType;
 
 @ExtendWith(MockitoExtension.class)
 public class TestServiceService {
@@ -33,6 +30,120 @@ public class TestServiceService {
 	@InjectMocks
 	private ServiceService serviceService;
 	
-	private static String SERVICE_KEY = "TestService";
+	private static ServiceType SERVICE_TYPE= ServiceType.CarRepair;
+	private static int PRICE = 340;
+	private static List<Mechanic> MECHANICS = new ArrayList<Mechanic>();
+	private static List<Appointment> APPOINTMENTS = new ArrayList<Appointment>();
+	
+	@BeforeEach
+	public void setMockOutput() {
+		
+		lenient().when(serviceDao.findByServiceType(any(Service.ServiceType.class))).thenAnswer((InvocationOnMock invocation) -> {
+			if (invocation.getArgument(0).equals(SERVICE_TYPE)) {
+				Service service = new Service();
+				service.setServiceType(SERVICE_TYPE);
+				service.setPrice(PRICE);
+				service.setAppointments(APPOINTMENTS);
+				service.setMechanics(MECHANICS);
+				return service;
+			} else {
+				return null;
+			}
+		});
+		
+		Answer<?> returnParameterAsAnswer = (InvocationOnMock invocation) -> {
+			return invocation.getArgument(0);
+		};
+		
+		lenient().when(serviceDao.save(any(Service.class))).thenAnswer(returnParameterAsAnswer);	
+	}
+	
+	@Test
+	/**
+	 * Verifies the creation of a service object
+	 */
+	public void testCreateService()
+	{
+		
+		ServiceType type = ServiceType.RegularCheckup;
+		List<Mechanic> mechanics = new ArrayList<Mechanic>();
+		List<Appointment> appointments = new ArrayList<Appointment>();
+		int price = 142;
+		Service service = null;
+		try {
+			service = serviceService.createService(type, price, mechanics, appointments);
+		}catch(IllegalArgumentException e)
+		{
+			fail();
+		}
+		
+		assertNotNull(service);
+		assertEquals(type, service.getServiceType());
+	}
 
+	/**
+	 * Verifies that the service type of a service object is not null
+	 */
+	public void testCreateServiceTypeNull()
+	{
+		
+		ServiceType type = null;
+		List<Mechanic> mechanics = new ArrayList<Mechanic>();
+		List<Appointment> appointments = new ArrayList<Appointment>();
+		int price = 142;
+		Service service = null;
+		String error = null;
+		
+		try {
+			service = serviceService.createService(type, price, mechanics, appointments);
+		}catch(IllegalArgumentException e){
+			error = e.getMessage();
+		}
+		
+		assertNull(service);
+		assertEquals("Service type cannot be null", error);
+	}
+	
+	public void testCreateMechanicsNull()
+	{
+		
+		ServiceType type = ServiceType.RoadsideAssistance;
+		List<Mechanic> mechanics = null;
+		List<Appointment> appointments = new ArrayList<Appointment>();
+		int price = 142;
+		Service service = null;
+		String error = null;
+		
+		try {
+			service = serviceService.createService(type, price, mechanics, appointments);
+		}catch(IllegalArgumentException e){
+			error = e.getMessage();
+		}
+		
+		assertNull(service);
+		assertEquals("List of Mechanics cannot be null", error);
+	}
+	
+	/**
+	 * Verifies that the list of appointments associated to a particular service is not null
+	 */
+	public void testCreateAppointmentsNull()
+	{
+		
+		ServiceType type = ServiceType.RoadsideAssistance;
+		List<Mechanic> mechanics = new ArrayList<Mechanic>();
+		List<Appointment> appointments = null;
+		int price = 142;
+		Service service = null;
+		String error = null;
+		
+		try {
+			service = serviceService.createService(type, price, mechanics, appointments);
+		}catch(IllegalArgumentException e){
+			error = e.getMessage();
+		}
+		
+		assertNull(service);
+		assertEquals("List of Appointments cannot be null", error);
+	}
 }
