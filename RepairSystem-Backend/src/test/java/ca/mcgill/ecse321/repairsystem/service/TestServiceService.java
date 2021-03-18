3da3aea2a2078f9361bcf72e2f6c97ae41e6c696
SiaH319 <59781764+SiaH319@users.noreply.github.com>
2021-03-18 18:53:58 -0400

    service unit testing done (coverage: 95)

--- RepairSystem-Backend/src/test/java/ca/mcgill/ecse321/repairsystem/service/TestServiceService.java
+++ RepairSystem-Backend/src/test/java/ca/mcgill/ecse321/repairsystem/service/TestServiceService.java
@@ -29,15 +29,20 @@ public class TestServiceService {
 
 	@InjectMocks
 	private ServiceService serviceService;
-	
+
 	private static ServiceType SERVICE_TYPE= ServiceType.CarRepair;
 	private static int PRICE = 340;
 	private static List<Mechanic> MECHANICS = new ArrayList<Mechanic>();
 	private static List<Appointment> APPOINTMENTS = new ArrayList<Appointment>();
-	
+
+
+	private static ServiceType SERVICE_TYPE2= ServiceType.CarRepair;
+	private static int PRICE2 = 342;
+	private static List<Mechanic> MECHANICS2 = new ArrayList<Mechanic>();
+	private static List<Appointment> APPOINTMENTS2 = new ArrayList<Appointment>();
 	@BeforeEach
 	public void setMockOutput() {
-		
+
 		lenient().when(serviceDao.findByServiceType(any(Service.ServiceType.class))).thenAnswer((InvocationOnMock invocation) -> {
 			if (invocation.getArgument(0).equals(SERVICE_TYPE)) {
 				Service service = new Service();
@@ -50,21 +55,43 @@ public class TestServiceService {
 				return null;
 			}
 		});
-		
+
+
+		lenient().when(serviceDao.findAll()).thenAnswer((InvocationOnMock invocation) -> {
+			List<Service> services = new ArrayList<Service>();
+			Service service1 = new Service();
+			Service service2 = new Service();
+
+			service1.setServiceType(SERVICE_TYPE);
+			service1.setPrice(PRICE);
+			service1.setAppointments(APPOINTMENTS);
+			service1.setMechanics(MECHANICS);
+			services.add(service1);
+
+			service2.setServiceType(SERVICE_TYPE2);
+			service2.setPrice(PRICE2);
+			service2.setAppointments(APPOINTMENTS2);
+			service2.setMechanics(MECHANICS2);
+			services.add(service2);
+
+			return services;
+		});
+
+
 		Answer<?> returnParameterAsAnswer = (InvocationOnMock invocation) -> {
 			return invocation.getArgument(0);
 		};
-		
+
 		lenient().when(serviceDao.save(any(Service.class))).thenAnswer(returnParameterAsAnswer);	
 	}
-	
+
 	@Test
 	/**
 	 * Verifies the creation of a service object
 	 */
 	public void testCreateService()
 	{
-		
+
 		ServiceType type = ServiceType.RegularCheckup;
 		List<Mechanic> mechanics = new ArrayList<Mechanic>();
 		List<Appointment> appointments = new ArrayList<Appointment>();
@@ -76,7 +103,7 @@ public class TestServiceService {
 		{
 			fail();
 		}
-		
+
 		assertNotNull(service);
 		assertEquals(type, service.getServiceType());
 		assertEquals(mechanics, service.getMechanics());
@@ -89,22 +116,102 @@ public class TestServiceService {
 	 */
 	public void testCreateServiceTypeNull()
 	{
-		
+
 		ServiceType type = null;
 		List<Mechanic> mechanics = new ArrayList<Mechanic>();
 		List<Appointment> appointments = new ArrayList<Appointment>();
 		int price = 142;
 		Service service = null;
 		String error = null;
-		
+
 		try {
 			service = serviceService.createService(type, price);
 		}catch(IllegalArgumentException e){
 			error = e.getMessage();
 		}
-		
+
 		assertNull(service);
 		assertEquals("Service type cannot be null", error);
 	}
-	
+
+	@Test
+	public void testGetAllServiceType() {
+
+		ServiceType type = ServiceType.RegularCheckup;
+		List<Mechanic> mechanics = new ArrayList<Mechanic>();
+		List<Appointment> appointments = new ArrayList<Appointment>();
+		int price = 142;
+		Service service = serviceService.createService(type, price);
+
+		ServiceType type2 = ServiceType.RegularCheckup;
+		List<Mechanic> mechanics2 = new ArrayList<Mechanic>();
+		List<Appointment> appointments2 = new ArrayList<Appointment>();
+		int price2 = 143;
+		Service service2 = serviceService.createService(type, price);
+
+		String error = null;
+		List<Service> services = new ArrayList<Service>();
+
+		SERVICE_TYPE= type;
+		PRICE = price;
+		MECHANICS = mechanics;
+		APPOINTMENTS = appointments;
+		SERVICE_TYPE2= type2;
+		PRICE2 = price2;
+		MECHANICS2 = mechanics2;
+		APPOINTMENTS2 = appointments2;
+		try {
+			services = serviceService.getAllServices();
+		}
+		catch(IllegalArgumentException e)
+		{
+			error = e.getMessage();
+		}
+
+		assertNotNull(services.get(0));
+		assertEquals(services.get(0).getAppointments(),appointments);
+		assertEquals(services.get(0).getMechanics(), mechanics);
+		assertEquals(services.get(0).getPrice(), price);
+		assertEquals(services.get(0).getServiceType(), type);
+
+		assertNotNull(services.get(1));
+		assertEquals(services.get(1).getAppointments(),appointments2);
+		assertEquals(services.get(1).getMechanics(), mechanics2);
+		assertEquals(services.get(1).getPrice(), price2);
+		assertEquals(services.get(1).getServiceType(), type2);
+
+
+	}
+
+	@Test
+	public void testGetServiceByServiceType() {
+		ServiceType type = ServiceType.RegularCheckup;
+		List<Mechanic> mechanics = new ArrayList<Mechanic>();
+		List<Appointment> appointments = new ArrayList<Appointment>();
+		int price = 142;
+		Service service = serviceService.createService(type, price);
+		String error = null;
+
+		SERVICE_TYPE= type;
+		PRICE = price;
+		MECHANICS = mechanics;
+		APPOINTMENTS = appointments;
+
+		try {
+			service = serviceService.getServiceByServiceType(type);
+		}
+		catch(IllegalArgumentException e)
+		{
+			error = e.getMessage();
+		}
+
+		assertNotNull(service);
+		assertEquals(service.getAppointments(), appointments);
+		assertEquals(service.getMechanics(), mechanics);
+		assertEquals(service.getPrice(), price);
+		assertEquals(service.getServiceType(), type);
+
+
+	}
+
 }
