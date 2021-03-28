2a20452d25b4d0d2d759a086e1e914e661283fd1
Annabelle <35576910+annabelledion@users.noreply.github.com>
2021-03-28 17:56:08 -0400

    interface complete

--- RepairSystem-Frontend/src/pages/Welcome/AdminEditProfile.vue
+++ RepairSystem-Frontend/src/pages/Welcome/AdminEditProfile.vue
@@ -1,7 +1,44 @@
 <template>
     <div class="profile">
-        This is a dummy Edit Account Pageeee
-    </div>
+        <div class="name">
+             <div class="text">
+            Admin Name
+            </div>
+            <div class="ellipse">
+            </div>
+        </div>
+
+        <div class="container">
+        <div class="row">
+        <div class="col">Residence</div>
+        <div class="w-100"></div>
+        <input type="address" value="address" disabled></input>
+        <div class="w-100"></div>
+        <div class="col">Email</div>
+        <div class="w-100"></div>
+        <input type="email" value="email" disabled></input>
+        <div class="w-100"></div>
+        <div class="col">Telephone</div>
+        <div class="w-100"></div>
+        <input type="telephone" value="phone number" disabled></input>
+        </div>
+        </div>
+
+        <div class="container2">
+        <div class="row">
+        <div class="col">Password</div>
+        <div class="w-100"></div>
+        <input type="password" value="password" disabled> </input>
+        </div>
+        </div>
+
+        <div class="container3">
+                <font class = "button" align = "right"> Edit Information</font>
+        </div>
+
+        
+
+    </div>     
 </template>
 
 <script>
@@ -12,13 +49,72 @@
 
 <style scoped>
 .profile {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-    width: 100%;
-    font-size: 40px;
-    color: rgb(167, 167, 167);
-    font-weight: 600;
+    position: relative;
+    width: 738px;
+    height: 374px;
+    background: #FFFFFF;
+}
+.name{
+position: absolute;
+width: 266px;
+height: 65px;
+left: 19px;
+top: 9px;
+}
+.text{
+position: absolute;
+width: 190px;
+height: 38px;
+top: 17px;
+left: 95px;
+font-family: Roboto;
+font-style: normal;
+font-weight: 900;
+font-size: 26px;
+line-height: 38px;
+
+color: #37447E;
+}
+.ellipse{
+    border-radius: 50%;
+    position: absolute;
+    width: 65px;
+    height: 65px;
+    left: 19px;
+    top: 9px;
+    background: #C4C4C4;
+}
+.container{
+position: absolute;
+left: 43px;
+top: 87px;
+font-family: Roboto;
+font-style: normal;
+font-weight: 900;
+font-size: 15px;
+line-height: 38px;
+color: #37447E;
+}
+
+.container2{
+position: absolute;
+left: 285px;
+top: 87px;
+font-family: Roboto;
+font-style: normal;
+font-weight: 900;
+font-size: 15px;
+line-height: 38px;
+color: #37447E;
+}
+
+.container3 {
+    position: absolute;
+    left: 500px;
+    top: 200px;
+  background: rgb(51 41 134);
+  color: white;
+  padding: 6px;
+  border-radius: 8px;
 }
 </style>
