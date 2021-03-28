9df9e5fb55628eedc249f353a3c8534b18e764cb
SiaH319 <59781764+SiaH319@users.noreply.github.com>
2021-03-27 22:42:02 -0400

    customer welcome page

---
+++ RepairSystem-Frontend/src/pages/Welcome/CustomerDashboard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="customerDashboard">
+        <Sidebar/>
+        <div class="content">
+            <router-view/>
+        </div>
+    </div>
+</template>
+
+<script>
+import Sidebar from '../../components/Sidebar'
+export default {
+    name: 'home',
+
+    components: {
+        Sidebar
+    }
+}
+</script>
+
+<style>
+.customerDashboard {
+    display: grid;
+    grid-template-columns: 1fr 5fr;
+    background-color: rgb(51 41 134);
+    height: 100vh;
+    width: 100vw;
+}
+
+.content {
+    background-color: #D3D2E1;
+    border-radius: 10px;
+    margin: 6px 6px 6px 0px;
+}
+</style>
--- RepairSystem-Frontend/src/router/index.js
+++ RepairSystem-Frontend/src/router/index.js
@@ -1,16 +1,40 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
 import Landing from '../pages/Landing/Landing.vue'
+import customerDashboard from '../pages/Welcome/CustomerDashboard'
 
+Vue.use(VueRouter)
 
-Vue.use(Router)
+const routes = [
+ {
+    path: '/',
+    name: '',
+    component: Landing
+  },
+  {
+    path: '/customerDashboard',
+    component: customerDashboard,
+    children: [
+      {
+        path: '',
+        component: () => import( '../pages/Welcome/CustomerOverview.vue')
+      },
+      {
+        path: '/customerDashboard/myAccount',
+        component: () => import('../pages/Welcome/CustomerEditProfile.vue')
+      },
+      {
+        path: '/customerDashboard/bookAppointment',
+        component: () => import('../pages/CustomerBookAppointment.vue')
+      }
+    ]
+  }
+]
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: '',
-      component: Landing
-    },
-  ]
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
 })
+
+export default router
