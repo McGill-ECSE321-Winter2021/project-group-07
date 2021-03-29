da83f84c048d8084d2c72c5d71619d5d15aaa9d1
SiaH319 <59781764+SiaH319@users.noreply.github.com>
2021-03-29 15:07:04 -0400

    logo (link to the landing page) added

--- RepairSystem-Frontend/src/pages/LogInAndSignUp/SignUp.vue
+++ RepairSystem-Frontend/src/pages/LogInAndSignUp/SignUp.vue
@@ -1,6 +1,9 @@
 <template>
+
     <div class="main">
-        Sign Up
+                <div style = "position: absolute; left: 5px; top: 10px"> &nbsp; <router-link to="/"><img src="../../assets/admin.png" width = "50px" length = "50px"></router-link>
+          </div>
+        <center> Sign Up </center>
         <div class="leftCar">
             <img src="../../assets/LogInCarLeft.svg" alt="Left Car" width="600" height="430">
         </div>
@@ -15,26 +18,26 @@
             </svg>
         </div>
 
-        <div class="textBox">
-            <form>
-                <label for="email">Email: </label>
+        <div class="textBox" >
+            <form > 
+               &nbsp;&nbsp; <label for="email">Email: </label>
                 <input type="text" v-model="newEmail" value=""><br>
-                <label for="password">Password:</label>
+               &nbsp;&nbsp; <label for="password">Password:</label>
                 <input type="text" v-model="newPassword" value=""><br>
-                <label for="repeat-password">Repeat Password:</label>
+               &nbsp;&nbsp; <label for="repeat-password">Repeat Password:</label>
                 <input type="text" v-model="newRepeatPassword" value=""><br>
-                <label for="name">Name:</label>
+               &nbsp;&nbsp; <label for="name">Name:</label>
                 <input type="text" v-model="newName" value=""><br>
-                <label for="number">Phone Number:</label>
+               &nbsp;&nbsp; <label for="number">Phone Number:</label>
                 <input type="text" v-model="newNumber" value=""><br>
-                <label for="address">Address:</label>
+               &nbsp;&nbsp; <label for="address">Address:</label>
                 <input type="text" v-model="newAddress" value=""><br>
-                <label for="credit">Credit:</label>
+               &nbsp;&nbsp; <label for="credit">Credit:</label>
                 <input type="text" v-model="newCredit" value=""><br>
-                <label for="debit">Debit:</label>
+               &nbsp;&nbsp; <label for="debit">Debit:</label>
                 <input type="text" v-model="newDebit" value=""><br>
             </form>
-            <button @click="createCustomer(newEmail, newPassword, newName, newNumber, newAddress, newCredit, newDebit)">Sign Up</button>
+             &nbsp;&nbsp;  <button class = "button1" @click="createCustomer(newEmail, newPassword, newName, newNumber, newAddress, newCredit, newDebit)">Sign Up</button>
         </div>
     </div>
 
@@ -64,7 +67,6 @@
 .textBox {
   position: absolute;
   top: 260px;
-  left: 330px;
   width: 800px;
   height: 200px;
 }
@@ -97,6 +99,14 @@ br {
 input {
   color: rgb(80, 95, 152);
   font-size: 12px;
+}
+.button1 {
+  background: rgb(51 41 134);
+  width: 80px;
+  height: 30px;
+  color: white;
+  padding: 2px;
+  border-radius: 8px;
 }
 
 
