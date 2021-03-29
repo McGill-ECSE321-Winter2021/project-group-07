26accbcbfaa5a46008be4cc86054455b878b1c18
SiaH319 <59781764+SiaH319@users.noreply.github.com>
2021-03-29 14:18:16 -0400

    edit profile frontend update

--- RepairSystem-Frontend/src/pages/Welcome/MechanicEditProfile.vue
+++ RepairSystem-Frontend/src/pages/Welcome/MechanicEditProfile.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="profile">
+    <div class = "row no-gutters">
+        <div class = "col no-gutters">
+        <div class="profile">
         <div class="name">
              <div class="text">
             Mechanic Name
@@ -40,23 +42,58 @@
                 <font class = "button" align = "right"> Edit Information</font>
         </div>
 
+
         
 
-    </div>     
+    </div> 
+        </div>
+        
+        <div class = "col no-gutters">
+            <div class = "rightside">
+                <div class = "row no-gutters">
+                    <div class = "top"> 
+                        <center><img src="../../assets/profile-default.png"   width = "100px" length = "100px" ></center>
+                        <br>
+                        <center> <b style = "color: rgb(51 41 134); font-size: 20px;"> medhanic name </b> </center>
+                    </div>
+                </div>
+
+                <div class = "row no-gutters" >
+                    <div class = "middle"> 
+                            <date-pick v-model="date" :hasInputElement="false"></date-pick>
+                    </div>
+                </div>
+                <div class = "row no-gutters" >
+                    <div class = "bottom"> 
+                        <img src="../../assets/paint job 1.jpg" width = "400px" length = "400px">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import DatePick from 'vue-date-pick';
+import 'vue-date-pick/dist/vueDatePick.css';
+
+export default {
+    components: {DatePick},
+};
 </script>
 
 <style scoped>
 .profile {
     position: relative;
-    width: 738px;
-    height: 374px;
-    background: #FFFFFF;
+    top: 20px;
+    right: -20px;
+    width: 115%;
+    height: 500px;
+    border-radius: 25px;
+    background: white;
+    border: 3px solid;
+    border-color: #D3D2E1;
+    padding: 25px;
 }
 .name{
 position: absolute;
@@ -90,8 +127,8 @@ color: #37447E;
 }
 .container{
 position: absolute;
-left: 43px;
-top: 87px;
+left: 80px;
+top: 120px;
 font-family: Roboto;
 font-style: normal;
 font-weight: 900;
@@ -102,8 +139,8 @@ color: #37447E;
 
 .container2{
 position: absolute;
-left: 285px;
-top: 87px;
+left: 305px;
+top: 120px;
 font-family: Roboto;
 font-style: normal;
 font-weight: 900;
@@ -121,4 +158,74 @@ color: #37447E;
   padding: 6px;
   border-radius: 8px;
 }
+.overview {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    font-size: 40px;
+    color: rgb(167, 167, 167);
+    font-weight: 600;
+}
+
+.rightside{
+    height: 98.5vh;
+    width: 70%;
+    position: absolute;
+    content: "";
+    right: 0px;
+    background: white
+}
+
+.top{
+    height: 50%;
+    width: 100%;
+    position: absolute;
+    content: "";
+    top: 20px;
+}
+
+.rcorners {
+  border-radius: 25px;
+  background: white;
+  width: 750px;
+  height: 150px;
+}
+
+
+.middle{
+    height: 30%;
+    width: 90%;
+    position: absolute;
+    content: "";
+    left:20px;
+    top: 200px;
+}
+
+.bottom{
+    height: 30%;
+    width: 80%;
+    position: absolute;
+    content: "";
+    left:20px;
+    top: 500px;
+}
+
+.left-top{
+
+    position: absolute;
+    content: "";
+    top: 50px;
+    left: 70px
+}
+
+.left-bottom{
+
+    position: absolute;
+    content: "";
+    left:70px;
+    top: 250px;
+}
+
 </style>
