28ef89c5c2d79162f9d63264f6210f01f41d0abf
norman-kong <normankong3@gmail.com>
2021-04-16 12:50:48 -0400

    deleting mail button

--- RepairSystem-Android/app/src/main/java/ca/mcgill/ecse321/repairsystem/ViewAppointments.java
+++ RepairSystem-Android/app/src/main/java/ca/mcgill/ecse321/repairsystem/ViewAppointments.java
@@ -29,16 +29,6 @@ public class ViewAppointments extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
         txtAppTime=(TextView)findViewById(R.id.appTime);
         txtServiceType=(TextView)findViewById(R.id.txtServiceType);
         txtMechanic=(TextView)findViewById(R.id.txtMechanic);
